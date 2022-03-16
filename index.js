3433854738cbca77674bb2d5d2dbc31b0f0356bb
MSoulSol <masrimizo@gmail.com>
2022-03-16 16:34:28 -0400

    building the question and answer prompts.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your Code Here
+let userName =window.prompt('Please enter your name:')
+for(let i=0; i<questions.length; i++){
+    console.log(questions[i])
+
+        let question = questions[i]
+        let userAnswer = window.prompt(questions[i].text)
+
+
+
+}
